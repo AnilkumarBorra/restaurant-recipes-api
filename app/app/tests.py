f5e0d39bd827ca48c39a20b0425788d5fe5c3dc9
Anil Kumar Borra <anilkumarborra5@gmail.com>
2024-07-06 16:58:26 +0530

    Added psycopg2 and configured django

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+    
+    def test_add_numbers(self):
+        """Test adding two numbers."""
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+        
+    def test_sub_number(self):
+        """Test subtract two numbers."""
+        res = calc.subtract(10,10)
+        self.assertEqual(res, 0)
